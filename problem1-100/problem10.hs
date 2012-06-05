6f8e6f250929fbb6477c5754bd5ad992af9e45df
mmito0310 <mmnwts@gmail.com>
2012-06-06 00:52:50 +0900

    Make faster problem 10

--- problem1-100/problem10.hs
+++ problem1-100/problem10.hs
@@ -1,6 +1,8 @@
+-- Todo: remove even numbers from x
 factors::Integer->[Integer]
-factors n = [ x | x <-[1..n], (mod n x) == 0 ]
+factors n = [ x | x <-[1..m], (mod n x) == 0 ]
+            where m = floor (sqrt $ fromIntegral n)
 prime::Integer->Bool
-prime n = (odd n) && (factors n == [1,n])
+prime n = (odd n) && (factors n == [1])
 primes:: Integer->Integer
 primes n = 2 + 3 + sum [ (6*x-1) | x <-[1..((n+1) `div` 6)], prime (6*x-1)] + sum [ (6*x+1) | x <-[1..((n-1) `div` 6)], prime (6*x+1)]
